4cfac9651228228164443d92a823e3e946e708d6
Gustavo Ferreira <gu.ferreira.9826@gmail.com>
2024-12-23 17:57:46 -0300

    Alguns comentários adicionados

--- src/script.js
+++ src/script.js
@@ -1,24 +1,25 @@
 $(document).ready(() => {
+  /*A função abaixo tem por finalidade fazer com que os elementos filhos da tag 
+    nav se comportem como um menu retrátil em dispositivos de menor resolução (=<789)*/
+  $(".nav-toggle").on("click", () => {
+    $("#nav-bar ul").slideToggle("medium");
+  });
 
-   $(".nav-toggle").on("click", () => {
-    $("#nav-bar ul").slideToggle("medium")
-   });
-   
-   $("#contact-form").on("submit", function(event) {
+  $("#contact-form").on("submit", function (event) {
     event.preventDefault();
 
-    if(!$("#name").val()) {
-        alert("Campo nome deve ser informado");
+    if (!$("#name").val()) {
+      alert("Campo nome deve ser informado");
     }
 
     const formData = {
-        name: $("#name").val(),
-        email: $("#email").val(),
-        subject: $("#subject").val(),
-        message: $("#message").val()
+      name: $("#name").val(),
+      email: $("#email").val(),
+      subject: $("#subject").val(),
+      message: $("#message").val(),
     };
 
-    console.log(formData)
+    console.log(formData);
 
     /*
     $.ajax({
@@ -33,5 +34,5 @@ $(document).ready(() => {
         }
     })
     */
-   });
+  });
 });
