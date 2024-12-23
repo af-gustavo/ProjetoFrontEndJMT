b1bcae8c035bc9dece1a76bd4e40f1d1cee25435
Gustavo Ferreira <gu.ferreira.9826@gmail.com>
2024-12-23 11:14:45 -0300

    Criados arquivos do projeto

---
+++ src/script.js
@@ -0,0 +1,37 @@
+$(document).ready(() => {
+
+   $(".nav-toggle").on("click", () => {
+    $("#nav-bar ul").slideToggle("medium")
+   });
+   
+   $("#contact-form").on("submit", function(event) {
+    event.preventDefault();
+
+    if(!$("#name").val()) {
+        alert("Campo nome deve ser informado");
+    }
+
+    const formData = {
+        name: $("#name").val(),
+        email: $("#email").val(),
+        subject: $("#subject").val(),
+        message: $("#message").val()
+    };
+
+    console.log(formData)
+
+    /*
+    $.ajax({
+        type:"POST",
+        url: "endereco-backend",
+        data: formData,
+        sucess: function(response) {
+            console.log("Fromulário enviado com sucesso");
+        },
+        error: function(error) {
+            console.log("Erro ao enviar o formulário", error);
+        }
+    })
+    */
+   });
+});
